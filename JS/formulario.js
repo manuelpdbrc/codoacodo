e1b792bd5b67517b68ef3319bace29a1a5b8e154
Jesica Moure <jesicamoure1@gmail.com>
2023-05-25 18:00:55 -0300

    validacion js
    se agregan validaciones

--- JS/formulario.js
+++ JS/formulario.js
@@ -5,6 +5,10 @@ formulario.addEventListener("submit", validar)
 function validar (e) {
     e.preventDefault()
     validarNombre(e)
+    validarApellido(e)
+    validarLocalidad(e)
+    validarEmail (e)
+    validarTitulo (e)
     validarCheckbox(e)
 
 }
@@ -16,6 +20,41 @@ function validarNombre (e) {
         e.preventDefault()
     }
 }
+
+function validarApellido (e) {
+    let apellidos = formulario.querySelector('[name="apellidos"]')
+    if (apellidos.value == 0){
+        alert("Ups, te falto escribir el apellido!")
+        e.preventDefault()
+    }
+}
+
+function validarLocalidad (e) {
+    let localidad = formulario.querySelector('[name="localidad"]')
+    if (localidad.value == 0){
+        alert("Para saber de donde viene tu creacion completa tu localidad!")
+        e.preventDefault()
+    }
+}
+
+function validarEmail (e) {
+    let email = formulario.querySelector('[name="email"]').value
+    let expReg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/;
+    if (expReg.test(email)){
+        alert("La dirección de email " + email + " es correcta.");
+       } else {
+        alert("La dirección de email es incorrecta.");
+       }
+}
+
+function validarTitulo (e) {
+    let titulo = formulario.querySelector('[name="titulo"]')
+    if (titulo.value == 0){
+        alert("No te olvides de colocar el titulo de tu plato!")
+        e.preventDefault
+    }
+}
+
 
 function validarCheckbox (e) {
     let terminos = formulario.querySelector(".terminosycondiciones")
