54b4301136d6b09c49305857c273d34c713e8f6d
Leo Mathot <113390921+leomathot@users.noreply.github.com>
2023-06-08 23:55:16 -0300

    Se dejan las temperaturas directamente visibles

--- JS/temperatura.js
+++ JS/temperatura.js
@@ -1,43 +1,62 @@
-let ciudades = document.querySelectorAll(".ciudad")
+const ciudadesYCoordenadas = [
+    {
+        "nombre": "Salta",
+        "latitud": -24.80,
+        "longitud": -65.42
+    },
+    {
+        "nombre": "Posadas",
+        "latitud": -27.38,
+        "longitud": -55.92
+    },
+    {
+        "nombre": "Santa Rosa",
+        "latitud": -36.61,
+        "longitud": -64.28
+    },
+    {
+        "nombre": "Rawson",
+        "latitud": -43.39,
+        "longitud": -65.08
+    },
+    {
+        "nombre": "Ushuaia",
+        "latitud": -54.96,
+        "longitud": -68.24
+    }
+]
 
-ciudades.forEach(elem => {
-    elem.addEventListener("click", () => {
-        let ciudad = elem.innerHTML
-        console.log(ciudad)
-        switch(ciudad) {
-            case "Salta":
-                latitud = -24.80
-                longitud = -65.42
-                break
-            case "Posadas":
-                latitud = -27.38
-                longitud = -55.92
-                break
-            case "Santa Rosa":
-                latitud = -36.61
-                longitud = -64.28
-                break;
-            case "Rawson":
-                latitud = -43.39
-                longitud = -65.08
-                break
-            case "Ushuaia":
-                latitud = -54.96
-                longitud = -68.24
-                break
-            default:
-                latitud = -31.40
-                longitud = -64.17
-        }
-        divDatos = document.querySelector('#datos')
-        fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitud}&longitude=${longitud}&current_weather=true`)
-            .then(res=>res.json())
-            .then(data=>{
-                console.log(data)
-                let temp = data.current_weather
-                .temperature
-                divDatos.innerHTML=`
-                    <h3>Temperatura en ${ciudad}: ${temp}º<h3>`    
-                })
-    })  
+const divTemperaturas = document.querySelector('#temperaturas')
+
+ciudadesYCoordenadas.forEach(ciudad => {
+
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${ciudad.latitud}&longitude=${ciudad.longitud}&current_weather=true`
+
+    fetch(url)
+                .then(res=>res.json())
+                .then(data=>{
+                    let temp = data.current_weather.temperature
+                    let prob
+                    if (temp < 10) {
+                        prob = "Muy alta"
+                        probClass = "prob-muy-alta"
+                    } else if (temp < 14) {
+                        prob = "Alta"
+                        probClass = "prob-alta"
+                    } else if (temp < 18) {
+                        prob = "Media"
+                        probClass = "prob-media"
+                    } else {
+                        prob = "Baja"
+                        probClass = "prob-baja"
+                    }
+                    divTemperaturas.innerHTML += `
+                        <div class="ciudad">
+                            <p>${ciudad.nombre}<p>
+                            <p class="temp">${temp}º</p>
+                            <p class="probab-texto">Probabilidad de guiso</p>
+                            <p class="probab-guiso ${probClass}">${prob}</p>
+                        </di>`
+                    })
+
 })
